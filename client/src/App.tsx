1cd9fa24745e6c95ac5dc9ea883669a6d75510de
Gabriel Lopez <gabo_71096@hotmail.com>
2023-08-08 11:51:23 +1200

    Feat: Make an API request to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import railsLogo from "/rails.png";
 import "./App.css";
+import axios from "axios";
+
+const url = "http://localhost:3000/api/v1";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState({ text: "" });
+
+  useEffect(() => {
+    axios.get(url).then((res) => setText(res.data));
+  }, []);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="inline logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="inline logo react" alt="React logo" />
+        </a>
+        <a href="https://rubyonrails.org/" target="_blank">
+          <img src={railsLogo} className="inline logo" alt="Rails logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1>Vite + React + Rails</h1>
+      <p>{text.text}</p>
     </>
   );
 }
