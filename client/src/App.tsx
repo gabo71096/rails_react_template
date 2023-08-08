61e3c02f4c6e7d9a19d61d27870308502bd41ce0
Gabriel Lopez <gabo_71096@hotmail.com>
2023-08-08 21:54:41 +0000

    Chore: Cleanup App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
-import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import { Outlet } from "react-router-dom";
-import { themeChange } from "theme-change";
-import { useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
-  useEffect(() => {
-    themeChange(false);
-  }, []);
-
   return (
-    <>
-      <select data-choose-theme className="border h-10 rounded">
-        <option value="">Default</option>
-        <option value="dark">Dark</option>
-      </select>
+    <div className="min-h-screen">
+      <Header />
       <Outlet />
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
